571143a141d0fd234de4502b8db1eacb490f83c6
Brendan Fisher <brendanfisher88@gmail.com>
2025-09-26 13:58:04 -0400

    UI Changes and implemented radio buttons changing the combo box

--- PrismaMediaManager/MainWindow.xaml.cs
+++ PrismaMediaManager/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -10,6 +9,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using Microsoft.Win32;
+using System.Diagnostics;
 
 namespace Prisma_Media_Manager;
 
@@ -28,5 +28,31 @@ public partial class MainWindow : Window
     {
         FFmpeg.CheckDependencies();
         yt_dlp.CheckDependencies();
+    }
+
+    private void ManageFormatBox(ComboBox c, string mode)
+    {
+        List<string> VIDEO_TYPES = [".mp4", ".avi", "mov", "flp", "webm"];
+        List<string> AUDIO_TYPES = [".mp3", ".wav", ".ogg", "opus", "aac", "flac"];
+
+        if(mode == "Video")
+            c.ItemsSource = VIDEO_TYPES;
+        else if(mode == "Audio")
+            c.ItemsSource = AUDIO_TYPES;
+
+        c.SelectedIndex = 0;
+    }
+
+    private void RadioButton_Checked(object sender, RoutedEventArgs e)
+    {
+        var radioButton = sender as RadioButton;
+        if (radioButton != null && radioButton.IsChecked == true)
+        {
+            string mode = radioButton.Content.ToString();
+            if (radioButton.GroupName == "DownloadOutputType")
+                ManageFormatBox(downloadFormat, mode);
+            else if (radioButton.GroupName == "ConvertOutputType")
+                ManageFormatBox(convertFormat, mode);
+        }
     }
 }
