8f9ece6eabb36b5113ed150d03dc6261958ea4bc
Brendan Fisher <brendanfisher88@gmail.com>
2025-09-26 19:54:49 -0400

    Implemented the ability to download videos
    Added the ability to download videos and audio in addition to adding the functionality to notify the user and open the file location upon clicking

--- PrismaMediaManager/MainWindow.xaml.cs
+++ PrismaMediaManager/MainWindow.xaml.cs
@@ -8,8 +8,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
-using Microsoft.Win32;
-using System.Diagnostics;
 
 namespace Prisma_Media_Manager;
 
@@ -32,8 +30,8 @@ public partial class MainWindow : Window
 
     private void ManageFormatBox(ComboBox c, string mode)
     {
-        List<string> VIDEO_TYPES = [".mp4", ".avi", "mov", "flp", "webm"];
-        List<string> AUDIO_TYPES = [".mp3", ".wav", ".ogg", "opus", "aac", "flac"];
+        List<string> VIDEO_TYPES = new List<string> { "mp4", "avi", "mov", "flp", "webm" };
+        List<string> AUDIO_TYPES = new List<string> { "mp3", "wav", "ogg", "opus", "aac", "flac" };
 
         if(mode == "Video")
             c.ItemsSource = VIDEO_TYPES;
@@ -54,5 +52,35 @@ public partial class MainWindow : Window
             else if (radioButton.GroupName == "ConvertOutputType")
                 ManageFormatBox(convertFormat, mode);
         }
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (ValidateDownloadFields())
+        {
+            string url = urlBox.Text;
+            string format = downloadFormat.Text;
+            var saveDialog = new Microsoft.Win32.SaveFileDialog();
+            saveDialog.DefaultExt = $".{format}";
+            saveDialog.Filter = $"Media file (.{format})|*.{format}";
+
+            if (saveDialog.ShowDialog() == true)
+            {
+                yt_dlp download = new yt_dlp(urlBox.Text, saveDialog.FileName, format);
+                download.DownloadVideo(debugBox);
+            }
+        }
+    }
+
+    private bool ValidateDownloadFields()
+    {
+        bool result = false;
+        if (urlBox.Text == "URL" || urlBox.Text.Replace(" ", "") == "")
+            MessageBox.Show("Please enter a URL!", "Prisma Media Manager", MessageBoxButton.OK, MessageBoxImage.Error);
+        else if (downloadFormat.Text == "URL" || downloadFormat.Text.Replace(" ", "") == "")
+            MessageBox.Show("Please select a file format!", "Prisma Media Manager", MessageBoxButton.OK, MessageBoxImage.Error);
+        else
+            result = true;
+        return result;
     }
 }
--- PrismaMediaManager/yt_dlp.cs
+++ PrismaMediaManager/yt_dlp.cs
@@ -6,13 +6,18 @@ using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace Prisma_Media_Manager
 {
     class yt_dlp
     {
+        //Static Variables
         const string DOWNLOAD_URL = "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe";
         static string exePath = Directory.GetCurrentDirectory() + @"\deps\yt-dlp.exe";
+        //Object variables
+        private string sourceUrl, outputPath, format;
+
         public static void CheckDependencies()
         {
             if (!File.Exists(exePath))
@@ -26,5 +31,59 @@ namespace Prisma_Media_Manager
                         "Prisma Media Manager", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
+        public yt_dlp(string url, string path, string type)
+        {
+            sourceUrl = url;
+            outputPath = path;
+            format = type;
+        }
+
+        public void DownloadVideo(System.Windows.Controls.TextBlock output)
+        {
+            string readError;
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.FileName = exePath;
+            startInfo.Arguments = $"{sourceUrl} --recode-video {format} -o \"{outputPath}\"";
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardError = true;
+            startInfo.RedirectStandardOutput = false;
+            process.StartInfo = startInfo;
+            process.Start();
+            StreamReader reader = process.StandardError;
+            process.WaitForExit();
+            if(File.Exists(outputPath))
+            {
+                string fileName = Path.GetFileNameWithoutExtension(outputPath);
+                new ToastContentBuilder()
+                    //.AddArgument("fileFolder", Path.GetDirectoryName(outputPath))
+                    .AddText("Download complete!")
+                    .AddText($"{fileName} has finished downloading successfully!")
+                    .Show();
+
+                ToastNotificationManagerCompat.OnActivated += toastArgs =>
+                {
+                    // Obtain the arguments from the notification
+                    //ToastArguments args = ToastArguments.Parse(toastArgs.Argument);
+
+                    // Need to dispatch to UI thread if performing UI operations
+                    Application.Current.Dispatcher.Invoke(delegate
+                    {
+                        // TODO: Show the corresponding content
+                        Process.Start("explorer.exe", $"/select,\"{outputPath}\"");
+                    });
+                };
+            }
+            else
+            {
+                readError = reader.ReadLine();
+                if (!(readError == null))
+                {
+                    MessageBox.Show(readError, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
     }
 }
