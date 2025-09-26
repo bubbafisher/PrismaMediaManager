714a220d0874050396ac23c638093e4f7eb8dffe
Brendan Fisher <brendanfisher88@gmail.com>
2025-09-26 11:58:36 -0400

    Changed the method of downloading yt-dlp

--- PrismaMediaManager/yt_dlp.cs
+++ PrismaMediaManager/yt_dlp.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +11,7 @@ namespace Prisma_Media_Manager
 {
     class yt_dlp
     {
+        const string DOWNLOAD_URL = "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe";
         static string exePath = Directory.GetCurrentDirectory() + @"\deps\yt-dlp.exe";
         public static void CheckDependencies()
         {
@@ -19,19 +20,10 @@ namespace Prisma_Media_Manager
                 var download = MessageBox.Show("yt-dlp.exe could not be found. Would you like to download it now?",
                     "Prisma Media Manager", MessageBoxButton.YesNo, MessageBoxImage.Error);
 
-                if(download == MessageBoxResult.Yes)
-                {
-                    using (var client = new WebClient())
-                    {
-                        client.DownloadFile("https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe",
-                            exePath);
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Please download yt-dl.exe and put it in " + Directory.GetCurrentDirectory() + @"\deps",
+                if (download == MessageBoxResult.Yes)
+                    Process.Start(new ProcessStartInfo(DOWNLOAD_URL) { UseShellExecute = true });
+                MessageBox.Show("Please download yt-dl.exe and put it in " + Directory.GetCurrentDirectory() + @"\deps",
                         "Prisma Media Manager", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
             }
         }
     }
